e9edd5e686edcbcc061e9d98bd35769f68351b67
PierreBarrat <p.barrat@live.fr>
2022-01-28 11:32:08 +0100

    changed fitness functions accordingly (now sum0 gauge)

--- src/fitness.jl
+++ src/fitness.jl
@@ -4,7 +4,7 @@ function fitness(xi::Integer, i::Integer, ϕ::AdditiveFitness)
 	if xi > 0 && ϕ.H[i] > 0. || xi < 0 && ϕ.H[i] < 0.
 		return abs(ϕ.H[i])
 	else
-		return 0.
+		return -abs(ϕ.H[i])
 	end
 end
 function fitness(x::Genotype, ϕ::AdditiveFitness)
@@ -30,7 +30,7 @@ function fitness(xi::Integer, i::Integer, ϕ::ExpiringFitness)
 	if xi > 0 && ϕ.H[i] > 0. || xi < 0 && ϕ.H[i] < 0.
 		return abs(ϕ.H[i]) * exp(-ϕ.α * ϕ.integrated_freq[i])
 	else
-		return 0.
+		return -abs(ϕ.H[i]) * exp(-ϕ.α * ϕ.integrated_freq[i])
 	end
 end
 
@@ -42,13 +42,15 @@ function fitness(x::Genotype, ϕ::PairwiseFitness)
 	for j in 1:length(x.seq)
 		if x.seq[j] > 0 && ϕ.H[j] > 0. || x.seq[j] < 0 && ϕ.H[j] < 0.
 			f += abs(ϕ.H[j])
+		else
+			f -= abs(ϕ.H[j])
 		end
 		for i in (j+1):length(x.seq)
 			# J[i,j] > 0 favors same state
 			# J[i,j] < 0 favors different states
-			if x.seq[i] == x.seq[j] && ϕ.J[i,j] > 0
+			if x.seq[i] == x.seq[j]
 				f += ϕ.J[i,j]
-			elseif x.seq[i] != x.seq[j] && ϕ.J[i,j] < 0
+			else
 				f -= ϕ.J[i,j]
 			end
 		end
