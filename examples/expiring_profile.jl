22d9ee6cc8f944c130f25362b74432c00c96f720
PierreBarrat <p.barrat@live.fr>
2023-06-06 16:42:20 +0200

    fix example

--- examples/expiring_profile.jl
+++ examples/expiring_profile.jl
@@ -19,19 +19,20 @@ begin
 	# T = 100
 
 	s = 0.05
+    H = s*ones(Float64, L)
 	α = -2*s/log(1-β)
 end
 
 Δt = Int(round(Int, .1/ρ)) # sample frequencies 10 times faster than ρ
 
 out = let
-	fitness_landscape = WF.ExpiringFitness(s, α, L)
+	fitness_landscape = WF.ExpiringFitness(; α, L, H)
 	pop = WF.Pop(fitness_landscape; N, L, μ)
 	out = WF.Tools.evolve_sample!(pop, 100, Δt, (div = WF.diversity,); switchgen = 1/ρ)
 end
 # compile run
 let
-	fitness_landscape = WF.ExpiringFitness(s, α, L)
+	fitness_landscape = WF.ExpiringFitness(; α, L, H)
 	pop = WF.Pop(fitness_landscape; N, L, μ)
 	frequencies, _, _ = WF.Tools.evolve_sample_freqs!(pop, 100, Δt; switchgen = 1/ρ)
 end;
@@ -39,14 +40,14 @@ end;
 
 
 begin
-	fitness_landscape = WF.ExpiringFitness(s, α, L)
+	fitness_landscape = WF.ExpiringFitness(; α, L, H)
 	pop = WF.Pop(fitness_landscape; N, L, μ)
 	Profile.clear()
 	@profile frequencies, _, _ = WF.Tools.evolve_sample_freqs!(pop, T, Δt; switchgen = 1/ρ)
 end;
 
 @btime let
-	fitness_landscape = WF.ExpiringFitness(s, α, L)
+	fitness_landscape = WF.ExpiringFitness(; α, L, H)
 	pop = WF.Pop(fitness_landscape; N, L, μ)
 	frequencies, _, _ = WF.Tools.evolve_sample_freqs!(pop, T, Δt; switchgen = 1/ρ)
 end;
